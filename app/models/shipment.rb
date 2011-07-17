096186cfd308e535a2c9945a95e3442673477706
Mark O'Shea <mark.oshea@yummimail.com>
2011-07-17 19:37:36 +0100

    near working

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,2 +1,7 @@
 class Shipment < ActiveRecord::Base
+  validates_numericality_of :height, :message => "can only be a number"
+  validates_numericality_of :width, :message => "can only be a number"
+  validates_numericality_of :depth, :message => "can only be a number"
+  validates_numericality_of :weight, :message => "can only be a number"
+  validates_inclusion_of :weight, :in => 0..30, :message => "can only weight between 0 and 30 kg"
 end
